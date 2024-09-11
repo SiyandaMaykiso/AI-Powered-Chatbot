0cceb6490841746cb6134d1c63a1926c78f81eb1
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-09-11 14:14:47 -0500

    Add logic

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ app.post('/chat', authMiddleware, async (req, res) => {
             {
                 model: 'gpt-3.5-turbo',
                 messages: [{ role: 'user', content: userQuery }],
-                max_tokens: 180,
+                max_tokens: 185,
                 temperature: 0.7,
             },
             {
